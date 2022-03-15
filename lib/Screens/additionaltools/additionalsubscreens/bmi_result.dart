15b0ef5b8b6501f16976e7c1d3075ff23de42b23
unknown <shahzeb.naqvi12@gmail.com>
2022-03-15 22:55:55 +0500

    bmi calculate result file

--- lib/Screens/additionaltools/additionalsubscreens/bmi_result.dart
+++ lib/Screens/additionaltools/additionalsubscreens/bmi_result.dart
@@ -36,6 +36,25 @@ class _Bmi_ResultState extends State<Bmi_Result> {
                   color: Constants.mainColor),
             ),
           ),
+          Container(
+              decoration: BoxDecoration(
+                color: Constants.lightColor,
+                borderRadius: BorderRadius.all(
+                    Radius.circular(MediaQuery.of(context).size.width * 0.03)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 1,
+                    blurRadius: 2,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              padding: EdgeInsets.all(
+                MediaQuery.of(context).size.width * 0.04,
+              ),
+              width: MediaQuery.of(context).size.width * 0.95,
+              child: Column(children: [Text("ss"), Text("ss"), Text("ss")])),
           Expanded(
             child: Align(
               alignment: Alignment.bottomCenter,
