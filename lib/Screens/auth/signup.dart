59456cf899d487da1c1a5aa7c9c937d0636223f2
MuhammadYousufbhimla <MuhammadYousufgemini@gmail.com>
2022-03-15 19:47:41 +0500

    Sign up form

--- lib/Screens/auth/signup.dart
+++ lib/Screens/auth/signup.dart
@@ -1,8 +1,11 @@
 import 'dart:ui';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:medical_app/Constants/constants.dart';
+import 'package:medical_app/Screens/auth/login.dart';
+
 class Signup extends StatelessWidget {
   const Signup({Key? key}) : super(key: key);
 
@@ -10,66 +13,98 @@ class Signup extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.all(10),
-        padding: EdgeInsets.all(10),
-        child: ListView(
-            children: [
-              Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+          child: ListView(children: [
+            Container(
               child: Image.network('assets/Sign_up.jpg'),
+            ),
+            Divider(
+              height: MediaQuery.of(context).size.height * 0,
+              thickness: 1,
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: Text("Sign Up",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      decorationColor: Constants.ligtbluecolor)),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                    labelText: 'Username',
+                    labelStyle: TextStyle(fontSize: 12)),
               ),
-               Divider(
-                height: MediaQuery.of(context).size.height * 0,
-                thickness: 1,
-                ), 
-                Container(
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.all(10),
-                child: Text("Sign Up",style:TextStyle(fontSize: 25,fontWeight: FontWeight.bold,decorationColor: Constants.ligtbluecolor)),
-                ),
-               
-                Container(  
-                 padding: EdgeInsets.all(10),
-                 child: TextField(
-                   decoration: InputDecoration(
-                     border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
-                     labelText: 'Username',
-                      labelStyle: TextStyle(fontSize: 12)
-                   ),
-                 ),
-                ),
-                  Container(
-                 padding: EdgeInsets.all(10),
-                 child: TextField(
-                   decoration: InputDecoration(
-                     border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
-                     labelText: 'Email',
-                     labelStyle: TextStyle(fontSize: 12)
-                   ),
-                 ),
-                ), 
-                  Container(
-                 padding: EdgeInsets.all(10),
-                 child: TextField(
-                   decoration: InputDecoration(
-                     border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
-                     labelText: 'Password ',
-                     labelStyle: TextStyle(fontSize: 12)
-                   ),
-                 ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  child: ElevatedButton(
-                    onPressed: (){},
-                  style: ElevatedButton.styleFrom(shape:StadiumBorder()),
-                  child: Text('Submit',style: TextStyle(fontSize:15,),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                    labelText: 'Email',
+                    labelStyle: TextStyle(fontSize: 12)),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                obscureText: true,
+                
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                    labelText: 'Password ',
+                    labelStyle: TextStyle(fontSize: 12)),
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.all(10),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(shape: StadiumBorder()),
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
                   ),
-                   )   )
-            ]
+                )
+                ),
+                Container(
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text:TextSpan(
+                      text: 'Already have an account?',
+                      style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w200),
+                            children:<TextSpan>[
+                              new TextSpan(
+                            text: 'Login in',
+                            recognizer: new TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Login())
+                            );
+                              },
+                          ),
+                            ]
+                    )
+                    ),
+                )
+
+          ]
           )
-          
-        ), 
-      
+          ),
+        
     );
   }
 }
