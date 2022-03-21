99f25c77d94a91e265a69ed8c2d9737c4cd65b79
unknown <shahzeb.naqvi12@gmail.com>
2022-03-22 00:13:40 +0500

    progressbar

---
+++ lib/Controllers/appointment_controller.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+import 'dart:convert';
+
+class AppointmentController extends GetxController {
+  var date = 0.obs;
+  var time = 0.obs;
+}
--- lib/Screens/maintools/maintoolssubscreens/bookappointment.dart
+++ lib/Screens/maintools/maintoolssubscreens/bookappointment.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:medical_app/Constants/constants.dart';
+import 'package:medical_app/Controllers/appointment_controller.dart';
 
 class BookApppointment extends StatefulWidget {
   const BookApppointment({Key? key}) : super(key: key);
@@ -7,6 +9,8 @@ class BookApppointment extends StatefulWidget {
   @override
   State<BookApppointment> createState() => _BookApppointmentState();
 }
+
+final appointmentController = Get.put(AppointmentController());
 
 class _BookApppointmentState extends State<BookApppointment> {
   @override
@@ -25,10 +29,134 @@ class _BookApppointmentState extends State<BookApppointment> {
               padding: EdgeInsets.only(
                 top: MediaQuery.of(context).size.height * 0.03,
               ),
-              child: Row(
-                children: [
-                  contdate(context, "29", "Sun", Colors.blue, Colors.white)
-                ],
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    InkWell(
+                        onTap: () {
+                          setState(() {});
+                          appointmentController.date.value = 1;
+                          print(appointmentController.date.value);
+                        },
+                        child: appointmentController.date.value == 1
+                            ? contdate(context, "29", "Sun",
+                                Constants.mainColor, Constants.lightColor)
+                            : contdate(context, "29", "Sun",
+                                Constants.lightColor, Constants.mainColor)),
+                    InkWell(
+                        onTap: () {
+                          setState(() {});
+                          appointmentController.date.value = 2;
+                          print(appointmentController.date.value);
+                        },
+                        child: appointmentController.date.value == 2
+                            ? contdate(context, "29", "Sun",
+                                Constants.mainColor, Constants.lightColor)
+                            : contdate(context, "29", "Sun",
+                                Constants.lightColor, Constants.mainColor)),
+                    InkWell(
+                        onTap: () {
+                          setState(() {});
+                          appointmentController.date.value = 3;
+                          print(appointmentController.date.value);
+                        },
+                        child: appointmentController.date.value == 3
+                            ? contdate(context, "29", "Sun",
+                                Constants.mainColor, Constants.lightColor)
+                            : contdate(context, "29", "Sun",
+                                Constants.lightColor, Constants.mainColor)),
+                    InkWell(
+                        onTap: () {
+                          setState(() {});
+                          appointmentController.date.value = 4;
+                          print(appointmentController.date.value);
+                        },
+                        child: appointmentController.date.value == 4
+                            ? contdate(context, "29", "Sun",
+                                Constants.mainColor, Constants.lightColor)
+                            : contdate(context, "29", "Sun",
+                                Constants.lightColor, Constants.mainColor)),
+                    InkWell(
+                        onTap: () {
+                          setState(() {});
+                          appointmentController.date.value = 5;
+                          print(appointmentController.date.value);
+                        },
+                        child: appointmentController.date.value == 5
+                            ? contdate(context, "29", "Sun",
+                                Constants.mainColor, Constants.lightColor)
+                            : contdate(context, "29", "Sun",
+                                Constants.lightColor, Constants.mainColor)),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.03,
+              ),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    InkWell(
+                        onTap: () {
+                          setState(() {});
+                          appointmentController.time.value = 1;
+                          print(appointmentController.time.value);
+                        },
+                        child: appointmentController.time.value == 1
+                            ? conttime(context, "29", "12:20 -03:20",
+                                Constants.mainColor, Constants.lightColor)
+                            : conttime(context, "29", "12:20 -03:20",
+                                Constants.lightColor, Constants.mainColor)),
+                    InkWell(
+                        onTap: () {
+                          setState(() {});
+                          appointmentController.time.value = 2;
+                          print(appointmentController.time.value);
+                        },
+                        child: appointmentController.time.value == 2
+                            ? conttime(context, "29", "12:20 -03:20",
+                                Constants.mainColor, Constants.lightColor)
+                            : conttime(context, "29", "12:20 -03:20",
+                                Constants.lightColor, Constants.mainColor)),
+                    InkWell(
+                        onTap: () {
+                          setState(() {});
+                          appointmentController.time.value = 3;
+                          print(appointmentController.time.value);
+                        },
+                        child: appointmentController.time.value == 3
+                            ? conttime(context, "29", "12:20 -03:20",
+                                Constants.mainColor, Constants.lightColor)
+                            : conttime(context, "29", "12:20 -03:20",
+                                Constants.lightColor, Constants.mainColor)),
+                    InkWell(
+                        onTap: () {
+                          setState(() {});
+                          appointmentController.time.value = 4;
+                          print(appointmentController.time.value);
+                        },
+                        child: appointmentController.time.value == 4
+                            ? conttime(context, "29", "12:20 -03 :20",
+                                Constants.mainColor, Constants.lightColor)
+                            : conttime(context, "29", "12:20 -03 :20",
+                                Constants.lightColor, Constants.mainColor)),
+                    InkWell(
+                        onTap: () {
+                          setState(() {});
+                          appointmentController.time.value = 5;
+                          print(appointmentController.time.value);
+                        },
+                        child: appointmentController.time.value == 5
+                            ? conttime(context, "29", "12:20 -03 :20",
+                                Constants.mainColor, Constants.lightColor)
+                            : conttime(context, "29", "12:20 -03 :20",
+                                Constants.lightColor, Constants.mainColor)),
+                  ],
+                ),
               ),
             ),
           ],
@@ -58,6 +186,7 @@ Widget contdate(context, date, day, colorcolor, fontcolor) {
         right: MediaQuery.of(context).size.width * 0.06,
         left: MediaQuery.of(context).size.width * 0.06,
         bottom: MediaQuery.of(context).size.height * 0.04),
+    margin: EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
     child: Column(
       children: [
         Text(
@@ -70,6 +199,47 @@ Widget contdate(context, date, day, colorcolor, fontcolor) {
             style: TextStyle(
                 color: fontcolor,
                 fontSize: MediaQuery.of(context).size.width * 0.08)),
+      ],
+    ),
+  );
+}
+
+Widget conttime(context, date, day, colorcolor, fontcolor) {
+  return Container(
+    decoration: BoxDecoration(
+      color: colorcolor,
+      borderRadius: BorderRadius.all(
+          Radius.circular(MediaQuery.of(context).size.width * 0.05)),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.2),
+          spreadRadius: 1,
+          blurRadius: 2,
+          offset: Offset(0, 3), // changes position of shadow
+        ),
+      ],
+    ),
+    padding: EdgeInsets.only(
+        top: MediaQuery.of(context).size.height * 0.04,
+        right: MediaQuery.of(context).size.width * 0.06,
+        left: MediaQuery.of(context).size.width * 0.06,
+        bottom: MediaQuery.of(context).size.height * 0.04),
+    margin: EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
+    child: Row(
+      children: [
+        Icon(
+          Icons.sunny,
+          color: fontcolor,
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.02,
+        ),
+        Text(
+          day,
+          style: TextStyle(
+              color: fontcolor,
+              fontSize: MediaQuery.of(context).size.width * 0.05),
+        ),
       ],
     ),
   );
