f68d5f6abf07c37e9b9280767116855d0fbfa87c
unknown <shahzeb.naqvi12@gmail.com>
2022-03-21 22:34:38 +0500

    progressbar

--- lib/Screens/maintools/maintoolssubscreens/bookappointment.dart
+++ lib/Screens/maintools/maintoolssubscreens/bookappointment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medical_app/Constants/constants.dart';
 
 class BookApppointment extends StatefulWidget {
   const BookApppointment({Key? key}) : super(key: key);
@@ -10,6 +11,66 @@ class BookApppointment extends StatefulWidget {
 class _BookApppointmentState extends State<BookApppointment> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Constants.mainColor,
+        title: Text('Book Appointments'),
+      ),
+      body: Center(
+          child: Container(
+        width: MediaQuery.of(context).size.width * 0.95,
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.05,
+              ),
+              child: Row(
+                children: [
+                  contdate(context, "29", "Sun", Colors.blue, Colors.white)
+                ],
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
   }
+}
+
+Widget contdate(context, date, day, colorcolor, fontcolor) {
+  return Container(
+    decoration: BoxDecoration(
+      color: colorcolor,
+      borderRadius: BorderRadius.all(
+          Radius.circular(MediaQuery.of(context).size.width * 0.05)),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.2),
+          spreadRadius: 1,
+          blurRadius: 2,
+          offset: Offset(0, 3), // changes position of shadow
+        ),
+      ],
+    ),
+    padding: EdgeInsets.only(
+        top: MediaQuery.of(context).size.height * 0.05,
+        right: MediaQuery.of(context).size.width * 0.05,
+        left: MediaQuery.of(context).size.width * 0.05,
+        bottom: MediaQuery.of(context).size.height * 0.05),
+    child: Column(
+      children: [
+        Text(
+          day,
+          style: TextStyle(
+              color: fontcolor,
+              fontSize: MediaQuery.of(context).size.width * 0.05),
+        ),
+        Text(date,
+            style: TextStyle(
+                color: fontcolor,
+                fontSize: MediaQuery.of(context).size.width * 0.05)),
+      ],
+    ),
+  );
 }
