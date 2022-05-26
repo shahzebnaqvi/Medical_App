5054e0a2454bd21135172f31f448f35ef6a80a24
Muhammad Yousuf <MuhammadYousufgemini@gmail.com>
2022-05-27 00:12:37 +0500

    create Login screen

--- lib/Screens/auth/signup.dart
+++ lib/Screens/auth/signup.dart
@@ -1,8 +1,10 @@
+import 'dart:html';
 import 'dart:ui';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 import 'package:medical_app/Constants/constants.dart';
 import 'package:medical_app/Screens/additionaltools/bmi.dart';
 import 'package:medical_app/Screens/auth/login.dart';
@@ -24,143 +26,273 @@ class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-            width: MediaQuery.of(context).size.width * 0.93,
-            child: ListView(children: [
-              Container(
-                  padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.06,
-                    bottom: MediaQuery.of(context).size.height * 0.09,
+        backgroundColor: Constants.mainColorWhite,
+        appBar: AppBar(
+          title: Text(
+            "Create Account",
+            style: TextStyle(color: Constants.blackcolor),
+          ),
+          leading: IconButton(
+              color: Colors.white,
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.black,
+                size: 20,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+        ),
+        body: ListView(children: [
+          Container(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.06,
+                bottom: MediaQuery.of(context).size.height * 0.09,
+              ),
+              alignment: Alignment.center,
+              child: Image.asset(
+                'assets/doctor2.jpg',
+                width: MediaQuery.of(context).size.width * 0.35,
+              )),
+          Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.01,
+                  bottom: MediaQuery.of(context).size.height * 0.01),
+              padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.02),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),
                   ),
-                  alignment: Alignment.bottomLeft,
-                  child: Image.asset(
-                    'assets/login.jpg',
-                    width: MediaQuery.of(context).size.width * 0.35,
-                  )),
-              // CircleAvatar(
-              //  child:
-              //  ),
-
-              Container(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  'Get Started',
-                  style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width * 0.1,
-                      fontWeight: FontWeight.bold),
-                ),
+                ],
               ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                    controller: username,
+              child: Column(
+                children: [
+                  TextField(
                     cursorColor: Constants.blackcolor,
                     decoration: InputDecoration(
-                      border: InputBorder.none,
-                      labelText: 'UserName',
-                    )),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                    controller: email,
-                    cursorColor: Constants.blackcolor,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      labelText: 'Email',
-                    )),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                    controller: password,
-                    cursorColor: Constants.blackcolor,
-                    obscureText: !this._showPassword,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      labelText: 'Password',
-                      suffixIcon: IconButton(
-                          icon: Icon(
-                            Icons.remove_red_eye,
-                            color: this._showPassword
-                                ? Constants.bluecolor
-                                : Constants.mediumColor,
-                          ),
-                          onPressed: () {
-                            setState(
-                                () => this._showPassword = !this._showPassword);
-                          }),
-                    )),
-              ),
-              Container(
-                alignment: Alignment.topRight,
-                width: MediaQuery.of(context).size.width * 0.3,
-                child: DropdownButton<String>(
-                  hint: _dropDownValue == ""
-                      ? Text('Gender')
-                      : Text(
-                          _dropDownValue,
-                          style: TextStyle(color: Constants.mainColor),
-                        ), // Not necessary for Option 1
-
-                  items: <String>['Male', 'Female'].map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      _dropDownValue = value!;
-                    });
-                  },
-                ),
-              ),
-              Container(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Constants.mainColor,
-                      minimumSize: Size(
-                          20,
-                          MediaQuery.of(context).size.height *
-                              0.06) // put the width and height you want
-                      ),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Login()));
-                  },
-                  child: Text(
-                    "Create Account",
-                    style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width * 0.05,
-                    ),
+                        border: InputBorder.none,
+                        labelText: "First Name",
+                        prefixIcon: Icon(Icons.person_outline)
+                        //suffixIcon: Icon(Icons.remove_red_eye)),
+                        ),
                   ),
-                ),
-              ),
+                  TextField(
+                    cursorColor: Constants.blackcolor,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        labelText: "Last Name",
+                        prefixIcon: Icon(Icons.person_outline)
+                        //suffixIcon: Icon(Icons.remove_red_eye)),
+                        ),
+                  ),
+                  TextField(
+                    cursorColor: Constants.blackcolor,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        labelText: "Email",
+                        prefixIcon: Icon(Icons.mark_email_unread_outlined)
+                        //suffixIcon: Icon(Icons.remove_red_eye)),
+                        ),
+                  ),
+                  TextField(
+                    cursorColor: Constants.blackcolor,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        labelText: "Password",
+                        prefixIcon: Icon(Icons.lock)
+                        //suffixIcon: Icon(Icons.remove_red_eye)),
+                        ),
+                  ),
+                ],
+              )),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              left: MediaQuery.of(context).size.height * 0.12,
+              right: MediaQuery.of(context).size.height * 0.12,
+            ),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  primary: Constants.bluecolor,
+                  minimumSize:
+                      Size(20, MediaQuery.of(context).size.height * 0.06)),
+              onPressed: () {},
+              child: Text("Sign up"),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.02,
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Already have an account?"),
+                GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Login()));
+                    },
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: Constants.bluecolor),
+                    ))
+              ],
+            ),
+          )
+        ])
 
-              Container(
-                padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.014),
-                child: RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                        text: 'Already have account? ',
-                        style: TextStyle(color: Constants.mainColor),
-                        children: <TextSpan>[
-                          new TextSpan(
-                            text: 'Sign up',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                            recognizer: new TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => Signup()));
-                              },
-                          ),
-                        ])),
-              ),
+        //         Container(
+        //           alignment: Alignment.bottomLeft,
+        //           child: Text(
+        //             'Get Started',
+        //             style: TextStyle(
+        //                 fontSize: MediaQuery.of(context).size.width * 0.1,
+        //                 fontWeight: FontWeight.bold),
+        //           ),
+        //         ),
+        //         Container(
+        //           padding: EdgeInsets.all(10),
+        //           child: TextField(
+        //               controller: username,
+        //               cursorColor: Constants.blackcolor,
+        //               decoration: InputDecoration(
+        //                 border: InputBorder.none,
+        //                 labelText: 'UserName',
+        //               )),
+        //         ),
+        //         Container(
+        //           padding: EdgeInsets.all(10),
+        //           child: TextField(
+        //               controller: email,
+        //               cursorColor: Constants.blackcolor,
+        //               decoration: InputDecoration(
+        //                 border: InputBorder.none,
+        //                 labelText: 'Email',
+        //               )),
+        //         ),
+        //         Container(
+        //           padding: EdgeInsets.all(10),
+        //           child: TextField(
+        //               controller: password,
+        //               cursorColor: Constants.blackcolor,
+        //               obscureText: !this._showPassword,
+        //               decoration: InputDecoration(
+        //                 border: InputBorder.none,
+        //                 labelText: 'Password',
+        //                 suffixIcon: IconButton(
+        //                     icon: Icon(
+        //                       Icons.remove_red_eye,
+        //                       color: this._showPassword
+        //                           ? Constants.bluecolor
+        //                           : Constants.mediumColor,
+        //                     ),
+        //                     onPressed: () {
+        //                       setState(
+        //                           () => this._showPassword = !this._showPassword);
+        //                     }),
+        //               )),
+        //         ),
+        //         Container(
+        //           alignment: Alignment.topRight,
+        //           width: MediaQuery.of(context).size.width * 0.3,
+        //           child: DropdownButton<String>(
+        //             hint: _dropDownValue == ""
+        //                 ? Text('Gender')
+        //                 : Text(
+        //                     _dropDownValue,
+        //                     style: TextStyle(color: Constants.mainColor),
+        //                   ), // Not necessary for Option 1
+
+        //             items: <String>['Male', 'Female'].map((String value) {
+        //               return DropdownMenuItem<String>(
+        //                 value: value,
+        //                 child: Text(value),
+        //               );
+        //             }).toList(),
+        //             onChanged: (value) {
+        //               setState(() {
+        //                 _dropDownValue = value!;
+        //               });
+        //             },
+        //           ),
+        //         ),
+        //         Container(
+        //           child: ElevatedButton(
+        //             style: ElevatedButton.styleFrom(
+        //                 primary: Constants.mainColor,
+        //                 minimumSize: Size(
+        //                     20,
+        //                     MediaQuery.of(context).size.height *
+        //                         0.06) // put the width and height you want
+        //                 ),
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Login()));
+        //             },
+        //             child: Text(
+        //               "Create Account",
+        //               style: TextStyle(
+        //                 fontSize: MediaQuery.of(context).size.width * 0.05,
+        //               ),
+        //             ),
+        //           ),
+        //         ),
+        //         Container(
+        //           padding: EdgeInsets.only(
+        //               top: MediaQuery.of(context).size.height * 0.014),
+        //           child: RichText(
+        //               textAlign: TextAlign.center,
+        //               text: TextSpan(
+        //                   text: 'Already have account? ',
+        //                   style: TextStyle(color: Constants.mainColor),
+        //                   children: <TextSpan>[
+        //                     new TextSpan(
+        //                       text: 'Sign up',
+        //                       style: TextStyle(fontWeight: FontWeight.bold),
+        //                       recognizer: new TapGestureRecognizer()
+        //                         ..onTap = () {
+        //                           Navigator.push(
+        //                               context,
+        //                               MaterialPageRoute(
+        //                                   builder: (context) => Signup()));
+        //                         },
+        //                     ),
+        //                   ])),
+        //         ),
+        //       ])),
+        // ),
+        );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
               // ElevatedButton.icon(
               //   icon: CircleAvatar(
               //       backgroundImage: AssetImage(
@@ -232,8 +364,4 @@ class _SignupState extends State<Signup> {
               // ],
               // ),
               // ),
-            ])),
-      ),
-    );
-  }
-}
+   
