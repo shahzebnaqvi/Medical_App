6c21c70097182e0c22510758e34bd0ed127add2a
Muhammad Yousuf <MuhammadYousufgemini@gmail.com>
2022-06-10 00:09:02 +0500

    carousel_slider add

--- lib/Screens/timelines.dart
+++ lib/Screens/timelines.dart
@@ -5,7 +5,6 @@ import 'package:medical_app/Screens/bottombar.dart';
 import '../Constants/constants.dart';
 import 'package:medical_app/Screens/bottombar.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-
 import 'carouselslider.dart';
 
 class Timelines extends StatefulWidget {
