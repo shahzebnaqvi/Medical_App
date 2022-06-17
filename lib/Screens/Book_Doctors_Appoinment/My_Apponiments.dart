df149321550e3edfd382374f8231eb0f9e129d2b
Muhammad Yousuf <MuhammadYousufgemini@gmail.com>
2022-06-17 21:12:10 +0500

    create map screen

--- lib/Screens/Book_Doctors_Appoinment/My_Apponiments.dart
+++ lib/Screens/Book_Doctors_Appoinment/My_Apponiments.dart
@@ -52,44 +52,47 @@ class _HospitalState extends State<MyAppointments> {
                       MaterialPageRoute(
                           builder: (context) => Doctors_Profiles()));
                 },
-                child: Hospitallocation(context, 'John Smith')),
-            Hospitallocation(context, 'Hamlet pham'),
-            Hospitallocation(context, 'Manji MyAppointments'),
-            Hospitallocation(context, 'Mary Hihi'),
-            Hospitallocation(context, 'Johny Doe'),
-            Hospitallocation(context, 'May Hampton'),
+                child: Appointment(
+                    context, ' Appointment', 'Accepted', 'Dr.Ethel Howard')),
+            Appointment(
+                context, 'Hamlet pham', 'Still in Progress ', 'Dr.John Ray'),
+            Appointment(
+                context, 'Manji MyAppointments', 'Rejected', 'Dr.Ethel Howard'),
+            Appointment(context, 'Mary Hihi', 'Accepted', 'Dr.John Ray'),
+            Appointment(context, 'Johny Doe', 'Unconfirmed', 'Dr.John Ray'),
+            Appointment(context, 'May Hampton', 'Accepted', 'Dr.Ethel Howard'),
           ],
         ));
   }
 }
 
-Widget Hospitallocation(context, title) {
+Widget Appointment(context, title, title1, doctor) {
   return Container(
-    margin: EdgeInsets.only(
-      top: MediaQuery.of(context).size.height * 0.03,
-      bottom: MediaQuery.of(context).size.height * 0.01,
-      left: MediaQuery.of(context).size.height * 0.02,
-      right: MediaQuery.of(context).size.height * 0.02,
-    ),
-    padding: EdgeInsets.only(
-      top: MediaQuery.of(context).size.height * 0.015,
-      left: MediaQuery.of(context).size.height * 0.01,
-      right: MediaQuery.of(context).size.height * 0.01,
-      bottom: MediaQuery.of(context).size.height * 0.01,
-    ),
-    decoration: BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(10),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.grey.withOpacity(0.2),
-          spreadRadius: 2,
-          blurRadius: 7,
-          offset: Offset(0, 3),
-        ),
-      ],
-    ),
-    child: ListTile(
+      margin: EdgeInsets.only(
+        top: MediaQuery.of(context).size.height * 0.03,
+        bottom: MediaQuery.of(context).size.height * 0.01,
+        left: MediaQuery.of(context).size.height * 0.02,
+        right: MediaQuery.of(context).size.height * 0.02,
+      ),
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).size.height * 0.015,
+        left: MediaQuery.of(context).size.height * 0.01,
+        right: MediaQuery.of(context).size.height * 0.01,
+        bottom: MediaQuery.of(context).size.height * 0.01,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: ListTile(
         leading: ClipOval(
           child: CircleAvatar(
             radius: 25,
@@ -98,40 +101,52 @@ Widget Hospitallocation(context, title) {
             ),
           ),
         ),
-        title: Text(
-          title,
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Constants.blackcolor,
-              fontSize: 18),
+        title: Row(
+          children: [
+            Text(
+              title,
+              style:
+                  TextStyle(color: Constants.grey, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.05,
+            ),
+            Text(
+              title1,
+              style:
+                  TextStyle(color: Constants.grey, fontWeight: FontWeight.w500),
+            ),
+          ],
         ),
         subtitle: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "Cardiologist",
-              style:
-                  TextStyle(color: Constants.grey, fontWeight: FontWeight.w500),
+              doctor,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Constants.blackcolor,
+                  fontSize: 18),
             ),
             Text("0.8 km away"),
           ],
         ),
-        trailing: Wrap(
-          spacing: 2,
-          children: [
-            Icon(
-              Icons.star,
-              color: Constants.ubl,
-              size: 18,
-            ),
-            Text(
-              "5.0",
-              style: TextStyle(
-                  color: Constants.ubl,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w600),
-            ),
-          ],
-        )),
-  );
+        // trailing: Wrap(
+        //   spacing: 2,
+        //   children: [
+        //     Icon(
+        //       Icons.star,
+        //       color: Constants.ubl,
+        //       size: 18,
+        //     ),
+        //     Text(
+        //       "5.0",
+        //       style: TextStyle(
+        //           color: Constants.ubl,
+        //           fontSize: 15,
+        //           fontWeight: FontWeight.w600),
+        //     ),
+        //   ],
+        // )),
+      ));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:medical_app/Screens/Book_Doctors_Appoinment/Doctors.dart';
-import 'package:medical_app/Screens/Book_Doctors_Appoinment/Hospital.dart';
-import 'package:medical_app/Screens/Book_Doctors_Appoinment/map.dart';
-import 'package:medical_app/Screens/Health%20&%20Fitness%20Goal/test_indicators.dart';
-import 'package:medical_app/Screens/additionaltools/bp.dart';
-import 'package:medical_app/Screens/auth/login.dart';
+
 import 'package:flutter/services.dart';
-import 'package:medical_app/Screens/auth/myprofile.dart';
-import 'package:medical_app/Screens/auth/signup.dart';
-import 'package:medical_app/Screens/bottombar.dart';
-import 'package:medical_app/Screens/home.dart';
-import 'package:medical_app/Screens/maintools/maintoolssubscreens/bookappointment.dart';
 
-import 'package:medical_app/testing.dart';
-
-import 'Screens/Book_Doctors_Appoinment/Book_apponiment.dart';
-import 'Screens/Book_Doctors_Appoinment/Doctors_Profile.dart';
-import 'Screens/auth/recipes.dart';
-import 'Screens/maintools/appointments.dart';
+import 'Screens/Book_Doctors_Appoinment/My_Apponiments.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +18,7 @@ class MyApp extends StatelessWidget {
     ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Book_Appointment(),
+      home: MyAppointments(),
     );
   }
 }
