a8a4e2c8ac08caee4c42bca1d7d9f75afdae03ba
Muhammad Yousuf <MuhammadYousufgemini@gmail.com>
2022-07-05 23:34:04 +0500

    Pill Remainder screen add

--- lib/Screens/Pill_Planner/Pill_Remainder.dart
+++ lib/Screens/Pill_Planner/Pill_Remainder.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import '../../Constants/constants.dart';
@@ -10,6 +11,12 @@ class PillRemainder extends StatefulWidget {
 }
 
 class _PillRemainderState extends State<PillRemainder> {
+  final List<String> imagesList = [
+    'assets/shop1.jpg',
+    'assets/shop1.jpg',
+    'assets/shop1.jpg',
+    'assets/shop1.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,8 +58,79 @@ class _PillRemainderState extends State<PillRemainder> {
             ), //Text
           ),
         ),
-        Container()
+        Container(
+          margin: EdgeInsets.only(
+            top: MediaQuery.of(context).size.height * 0.03,
+            bottom: MediaQuery.of(context).size.height * 0.01,
+            left: MediaQuery.of(context).size.height * 0.02,
+            right: MediaQuery.of(context).size.height * 0.02,
+          ),
+          padding: EdgeInsets.only(
+            top: MediaQuery.of(context).size.height * 0.015,
+            left: MediaQuery.of(context).size.height * 0.02,
+            right: MediaQuery.of(context).size.height * 0.02,
+            bottom: MediaQuery.of(context).size.height * 0.01,
+          ),
+          decoration: BoxDecoration(
+            color: Constants.bluecolor,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 2,
+                blurRadius: 7,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Center(
+              child: Text(
+            "My Mart",
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 25),
+          )),
+        ),
+        // Shop Slider
+        shopcrausel(),
       ]),
+    );
+  }
+
+// Shop CarouselSlider
+  Widget shopcrausel() {
+    return CarouselSlider(
+      options: CarouselOptions(
+        autoPlay: true,
+        enlargeCenterPage: true,
+        scrollDirection: Axis.horizontal,
+      ),
+      items: imagesList
+          .map(
+            (item) => Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: Card(
+                elevation: 6.0,
+                shadowColor: Constants.grey,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      Image.network(
+                        item,
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+          .toList(),
     );
   }
 }
